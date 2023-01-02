c230d6a0b21672051677f23e5e04b9bbcb3c4df3
Minh Le <qminh279@gmail.com>
2023-01-02 14:43:50 -0500

    submit 2.9

--- Part 2/phonebook/src/App.js
+++ Part 2/phonebook/src/App.js
@@ -2,13 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas',
-      number: '040-1234567' 
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNum, setNum] = useState('')
+  const [search, setSearch] = useState('')
 
   const handleChangeName = (event) => {
     setNewName(event.target.value)
@@ -18,6 +19,12 @@ const App = () => {
     setNum(event.target.value)
   }
 
+  const handleSearch = (event) => {
+    setSearch(event.target.value)
+  }
+
+  const checkSearch = (person) => person.name.toLowerCase().includes(search.toLowerCase())
+
   const nameArr = persons.map(person => person.name)
   const addNew = (event) => {
     event.preventDefault()
@@ -25,7 +32,7 @@ const App = () => {
       alert(`${newName} is already added to the phonebook`)
     }
     else{
-      const newDude = {name: newName, number: newNum}
+      const newDude = {name: newName, number: newNum, id: nameArr.length + 1}
       setPersons(persons.concat(newDude))
       setNewName('')
       setNum('')
@@ -35,6 +42,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with
+        <input value={search} onChange={handleSearch} />
+      </div>
+      <h3>add a new</h3>
       <form onSubmit={addNew}>
         <div>
           name: <input value={newName} onChange={handleChangeName} />
@@ -48,7 +60,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
+        {persons.filter((person) => checkSearch(person)).map(person => <li key={person.id}>{person.name} {person.number}</li>)}
       </ul>
     </div>
   )
