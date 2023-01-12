bdf0f35d27134676b1a8f679a7d0da30d7019ce2
Minh Le <qminh279@gmail.com>
2023-01-12 15:13:05 -0500

    submit 3.6

--- Part 3/index.js
+++ Part 3/index.js
@@ -55,10 +55,16 @@ const generateID = () => {
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
-    console.log(body)
+
     if (!body.name || !body.number){
         return response.status(400).json({
-            error: 'invalid content'
+            error: 'name or number is empty'
+        })
+    }
+
+    if (persons.find(person => person.name === body.name)){
+        return response.status(400).json({
+            error: 'name must be unique'
         })
     }
 
