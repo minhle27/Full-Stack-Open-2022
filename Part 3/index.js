ab21fad9afeb4b2b7cb5d2210561d69d6636809a
Minh Le <qminh279@gmail.com>
2023-01-13 12:19:59 -0500

    submit 3.8

--- Part 3/index.js
+++ Part 3/index.js
@@ -6,6 +6,10 @@ const morgan = require('morgan')
 app.use(express.json())
 app.use(morgan('tiny'))
 
+morgan.token('data', (req, res) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms -- :data'))
+
+
 let persons = [
     { 
         "id": 1,
