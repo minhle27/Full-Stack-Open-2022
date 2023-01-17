41de7580397ff7021fc432ab75db643672fd572d
Minh Le <qminh279@gmail.com>
2023-01-16 20:28:35 -0500

    submit 3.11

--- Part 3/index.js
+++ Part 3/index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 const morgan = require('morgan')
 
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
