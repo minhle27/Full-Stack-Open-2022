4050bab48ba04efc712cc37f74e8eceb77f95291
Minh Le <qminh279@gmail.com>
2023-01-18 14:04:19 -0500

    submit 3.17

--- Part 3/index.js
+++ Part 3/index.js
@@ -63,6 +63,20 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
 
 app.get('/info', (request, response) => {
     const info = {
