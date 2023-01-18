6db372e69a8a3b93857bfb979642f35867629c0a
Minh Le <qminh279@gmail.com>
2023-01-17 22:17:15 -0500

    add gitignore

--- Part 3/index.js
+++ Part 3/index.js
@@ -15,13 +15,6 @@ const url = `mongodb+srv://fullstack:<password>@cluster0.o1opl.mongodb.net/noteA
 
 mongoose.connect(url)
 
-const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
-    important: Boolean,
-  })
-
-const Note = mongoose.model('Note', noteSchema)
 
 let persons = [
     { 
