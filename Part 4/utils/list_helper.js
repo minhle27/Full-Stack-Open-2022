3da27191af926273bdd173700d3c10d3211ac0a2
Minh Le <qminh279@gmail.com>
2023-03-28 00:10:38 -0400

    submit 4.5

--- Part 4/utils/list_helper.js
+++ Part 4/utils/list_helper.js
@@ -10,7 +10,25 @@ const totalLikes = (array) => {
   return array.reduce(reducer, 0)
 }
 
+const favoriteBlog = (array) => {
+  if (array.length === 0) return {}
+  let index = 0
+  for (let i = 0; i < array.length; i++){
+    if (array[i].likes > array[index].likes){
+      index = i
+    }
+  }
+  
+  const res = {
+    'title': array[index].title,
+    'author': array[index].author,
+    'likes': array[index].likes
+  }
+  return res
+}
+
 module.exports = {
   dummy,
+  favoriteBlog,
   totalLikes
 }
