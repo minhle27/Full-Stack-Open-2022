e83e96d9246854ea3cc3dae554d4dbb9322e848e
Minh Le <qminh279@gmail.com>
2023-04-03 17:06:50 -0400

    Update Readme

--- Part 4/tests/blog_api.test.js
+++ Part 4/tests/blog_api.test.js
@@ -1,1 +1,28 @@
 const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+const api = supertest(app)
+
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    important: false,
+  },
+  {
+    content: 'Browser can execute only JavaScript',
+    important: true,
+  },
+]
+
+
+test('returns the correct amount of blog posts', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+}, 100000)
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
